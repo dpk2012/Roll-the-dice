4a4597eb83a38d2963fd6310df2431e7fd9c9c74
Your Name <you@example.com>
2021-02-19 15:34:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var img1="images/dice"+randomNumber1+".png";
+var img2="images/dice"+randomNumber2+".png";
+document.querySelector(".img1").setAttribute("src",img1);
+document.querySelector(".img2").setAttribute("src",img2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩 Player1 wins";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player2 wins 🚩";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw";
+}
